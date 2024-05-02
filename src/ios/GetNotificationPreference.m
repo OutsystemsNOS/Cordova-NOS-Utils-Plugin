f35eb54b4cc817ac484d872a85e853076d31e317
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-05-02 10:20:44 +0100

    Create GetNotificationPreference.m

---
+++ src/ios/GetNotificationPreference.m
@@ -0,0 +1,23 @@
+#import "GetNotificationPreference.h"
+#import "StoreKit/StoreKit.h"
+
+@implementation AppReviewPlugin
+
+- (void)getPreference:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult* pluginResult;
+
+    // Get the current notification settings
+    UIUserNotificationSettings *notificationSettings = [[UIApplication sharedApplication] currentUserNotificationSettings];
+    
+    // Check if the app has permission to display notifications
+    if (notificationSettings.types != UIUserNotificationTypeNone) {
+        NSLog(@"Notifications are enabled");
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        NSLog(@"Notifications are disabled");
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Notifications are disabled"];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
