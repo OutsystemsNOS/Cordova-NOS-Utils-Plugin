18312f7e89d1617124ed4f324a3af0628d8089af
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-08-05 16:57:42 +0100

    Update NOSUtilsPlugin.m

--- src/ios/NOSUtilsPlugin.m
+++ src/ios/NOSUtilsPlugin.m
@@ -19,4 +19,47 @@
     [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];    
 }
 
+- (void)callCameraPermission:(CDVInvokedUrlCommand *)command {
+    @try {
+        if (![self hasCameraPermission]) {
+            [self requestCameraPermissionWithCompletion:^(BOOL granted) {
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:granted];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }];
+        } else {
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+    }
+    @catch (NSException *exception) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:exception.reason];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }
+}
+
+- (void)requestCameraPermissionWithCompletion:(void (^)(BOOL granted))completion {
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            completion(granted);
+        });
+    }];
+}
+
+- (void)hasCameraPermission :(CDVInvokedUrlCommand *)command {
+    @try {
+        BOOL hasPermission = [self hasCameraPermission];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:hasPermission];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }
+    @catch (NSException *exception) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:exception.reason];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }  
+}
+
+- (BOOL)hasCameraPermission {
+    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    return authStatus == AVAuthorizationStatusAuthorized;
+}
+
 @end
