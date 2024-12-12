768aedf2c44fb26d1c97b9d80af65eb4636d3692
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-12-12 16:38:49 +0000

    Update NOSUtilsPlugin.java

--- src/android/NOSUtilsPlugin.java
+++ src/android/NOSUtilsPlugin.java
@@ -82,7 +82,8 @@ public class NOSUtilsPlugin extends CordovaPlugin {
       try {
           if (!PermissionHelper.hasPermission(this, Manifest.permission.RECEIVE_SMS) || !PermissionHelper.hasPermission(this, Manifest.permission.READ_SMS)) {           
               if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) { // Android 15 (API 34)
-                  ActivityCompat.requestPermissions(cordova.getActivity(), new String[]{Manifest.permission.RECEIVE_SENSITIVE_NOTIFICATIONS}, PERMISSION_REQUEST_READ_SMS);
+                  String sensitiveNotificationPermission = "android.permission.RECEIVE_SENSITIVE_NOTIFICATIONS";
+                  ActivityCompat.requestPermissions(cordova.getActivity(), new String[]{sensitiveNotificationPermission}, PERMISSION_REQUEST_READ_SMS);
               } else {
                   ActivityCompat.requestPermissions(cordova.getActivity(), new String[]{Manifest.permission.READ_SMS}, PERMISSION_REQUEST_READ_SMS);
               }
